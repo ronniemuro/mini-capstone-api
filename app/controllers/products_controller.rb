6da6b9ba9d88f064fb0a1d5efbbc5a101e9eed7b
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-05-04 22:03:29 +0200

    create action with body parameters and HTTP requests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,15 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: params["id"])
     render json: product.as_json
   end
+
+  def create
+    product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image"],
+      description: params["description"],
+    )
+    product.save
+    render json: product.as_json
+  end
 end
