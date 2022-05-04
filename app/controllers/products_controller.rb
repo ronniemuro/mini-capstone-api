de96b7a498f28d018c8eb2ec2578cd4253c8ec8b
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-05-04 22:36:40 +0200

    implemented update action in mini-capstone

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,4 +19,15 @@ class ProductsController < ApplicationController
     product.save
     render json: product.as_json
   end
+
+  def update
+    product_id = params["id"]
+    product = Product.find_by(id: product_id)
+    product.name = params["name"] || product.name
+    product.price = params["price"] || product.price
+    product.image_url = params["image_url"] || product.image_url
+    product.description = params["description"] || product.description
+    product.save
+    render json: product.as_json
+  end
 end
