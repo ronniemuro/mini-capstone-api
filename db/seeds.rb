46531e24c96d43fc31d5412c8cbd3e828b769e64
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-05-04 18:34:20 +0200

    initial products created in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create(name: "Blonde Wig", price: 19.69, image_url: "image here", description: "synthetic hair wig for any basic she/he/they/them")
+Product.create(name: "Red Wig", price: 39.94, image_url: "image here", description: "You trynna be Jinx Monsoon?")
+Product.create(name: "Pussy Cat Wig", price: 35.94, image_url: "image here", description: "You trynna be Monet X Change?")
