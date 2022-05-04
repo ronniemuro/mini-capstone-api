3f510d860f20aa03bc3d805c38df2601444c0dca
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-05-04 22:49:40 +0200

    destroy action implemented in mini-capstone

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,4 +30,11 @@ class ProductsController < ApplicationController
     product.save
     render json: product.as_json
   end
+
+  def destroy
+    product_id = params["id"]
+    product = Product.find_by(id: product_id)
+    product.destroy
+    render json: { message: "Product deleted" }
+  end
 end
