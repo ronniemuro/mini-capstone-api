c815eeb3f7c465c46500d21f56dea0d42eeaa2f6
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-05-10 21:03:37 +0200

    added seed dump gemfile to grab all DB into seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
-Product.create({ name: "Blonde Wig", price: 19.69, image_url: "https://cdn.shopify.com/s/files/1/0563/7170/7938/products/318pquO3bcL_1024x1024.jpg?v=1644005499", description: "synthetic hair wig for any basic she/he/they/them" })
-Product.create({ name: "Red Wig", price: 39.94, image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.jinkxanddela.com%2Fabout&psig=AOvVaw2qO1YGGFNMS-PGW-FYZ3G_&ust=1651768756014000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNisuLKkxvcCFQAAAAAdAAAAABAD", description: "You trynna be Jinx Monsoon?" })
-Product.create({ name: "Pussy Cat Wig", price: 35.94, image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Fdrag-babes--638596422138829486%2F&psig=AOvVaw3_w8x8eGNlJz5anfV57Y3o&ust=1651768825898000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOCKz-qkxvcCFQAAAAAdAAAAABAD", description: "You trynna be Monet X Change?" })
+Product.create!([
+  { name: "Bald", price: "0.0", image_url: "http://attitude.co.uk/media/images/2018/05/Sasha_crown_v3.jpg.750x400_q85_box-0%2C1%2C1000%2C535_crop_detail.jpg", description: "Bald queens", inventory_count: nil },
+  { name: "Pussy Cat Wig", price: "35.0", image_url: "https://64.media.tumblr.com/f18103da7c5e9eaffdbd47bf180fabd4/tumblr_inline_pnk88hc6Jj1r8hoy3_500.jpg", description: "You trynna be Monet X Change?", inventory_count: nil },
+  { name: "Red Wig", price: "39.0", image_url: "https://i.guim.co.uk/img/media/ec3c404caff621259266b59b38debeaacc3b275d/354_575_4363_2617/master/4363.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=df382b59c7549993f560cd1fa9fb31c5", description: "You trynna be Jinx Monsoon?", inventory_count: nil },
+  { name: "Blonde Wig", price: "19.0", image_url: "http://pm1.narvii.com/7244/6509c5c9b3cdd7ab3b4200e791380a0c18505d54r1-417-417v2_00.jpg", description: "synthetic hair wig for any basic she/he/they/them", inventory_count: nil },
+  { name: "Brunette Wig", price: "100.0", image_url: "https://pbs.twimg.com/media/DY0cfSPW0AEON_C.jpg", description: "You trynna be an iconic wig reveal?", inventory_count: nil },
+  { name: "Pink Wig", price: "15.0", image_url: "https://images.partydelights.co.uk/WIGS/69/1/front/v1/fac/4.jpg", description: "She done already done had herses", inventory_count: nil },
+])
